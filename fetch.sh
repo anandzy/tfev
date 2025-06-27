9fa7a7b1fa042cd3880b22db7ceebc2502c58d6d
Anand Bhupathiraju <anand.bhupathiraju@scania.com>
2025-06-27 12:55:36 +0200

    Check

--- fetch.sh
+++ fetch.sh
@@ -1,18 +1,36 @@
-VERSION="feature-feature%2F1a-3"  # You can make this dynamic!
+#!/bin/bash
+
+set -e
+
+VERSION="feature-feature%2F1a-4"
+
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+ARCH="amd64"
+if [[ "$OS" == "darwin" ]]; then
+  if sysctl -n machdep.cpu.brand_string | grep -q "Apple"; then
+    ARCH="arm64"
+  fi
+fi
+
 REPO="anandzy/tfev"
 BINARY="filtervars"
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-ARCH=$(uname -m)
-
-# Download URL
-URL="https://github.com/$REPO/releases/download/$VERSION/${BINARY}-${OS}-${ARCH}"
+URL="https://github.com/$REPO/releases/download/$VERSION/${BINARY}-$OS-$ARCH"
 
 echo "Downloading $BINARY version $VERSION for $OS/$ARCH..."
-curl -L "$URL" -o "$BINARY"
+echo "From: $URL"
+
+# Require GITHUB_TOKEN environment variable
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Error: Please export your GITHUB_TOKEN (GitHub personal access token) before running this script."
+  exit 1
+fi
+
+curl -sL -H "Authorization: token $GITHUB_TOKEN" "$URL" -o "$BINARY"
 chmod +x "$BINARY"
-sudo mv "$BINARY" /usr/local/bin/
 
-
-curl -L -v "https://github.com/anandzy/tfev/releases/download/$VERSION/filtervars-darwin-amd64" -o filtervars
-file filtervars
-head filtervars
+if ! ./"$BINARY" --version >/dev/null 2>&1; then
+  echo "Error: downloaded file is not a valid executable."
+  echo "Content of downloaded file:"
+  cat "$BINARY"
+  exit 1
+fi
